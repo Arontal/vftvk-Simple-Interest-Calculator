80488ae47feadf46cea955b5216a0b442e322ee5
GeoffreyArtis <Geoffrey.f.Artis@gmail.com>
2024-07-17 13:06:11 -0700

    Improved functionality between the Js and HTML

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+Interest.addEventListener("click", compute);
+
+function compute(){
+    let p = parseInt(document.getElementById("principal").value);
+    let r = parseFloat(document.getElementById("rate").value);
+    let y = parseInt(document.getElementById("years").value);
+
+    let result = p*r*y;
+
+    document.getElementById("result").innerHTML = result;
 }
         
